53e09d31c2b1d6a839c8582da93b0628e9114d9d
xavigm <xavi.coupe@gmail.com>
2022-08-16 21:09:50 +0200

    Added blog and elements render

--- main.py
+++ main.py
@@ -9,10 +9,29 @@ app = Flask(__name__, static_folder='')
 def index():
 
         templateData = {
-        'title': 'Text',
+        'title': 'Flask Skel',
     }
 
         return render_template('index.html', **templateData)
+
+@app.route('/generic.html')
+def generic():
+
+        templateData = {
+        'title': 'Generic',
+    }
+
+        return render_template('generic.html', **templateData)
+
+@app.route('/elements.html')
+def elements():
+
+        templateData = {
+        'title': 'Elements',
+    }
+
+        return render_template('elements.html', **templateData)
+
 
 # app.run() ##Replaced with below code to run it using waitress
 serve(app, host='127.0.0.1', port=80)  
